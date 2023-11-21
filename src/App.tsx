80ee74298d9439130c691e92dca27160a76f57b2
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-21 20:58:08 +0100

    Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App = () => {
       <Routes>
         {/* public routes */}
         <Route element={<AuthLayout />}>
-          <Route path='sign-in' element={<SigninForm />} />
-          <Route path='sign-up' element={<SignupForm />} />
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
         </Route>
 
         {/* private routes */}
