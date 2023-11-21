aec699b0fb4302493f98c5d15bef5d6cd363f7cd
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-21 20:44:31 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
 import './globals.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='sign-in' element={<SigninForm />} />
+          <Route path='sign-up' element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
