b3fd6001e8174bd724c5115e34556dd4d0193e8b
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-23 19:26:51 +0100

    Add route paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
-import { Home } from './_root/pages'
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
 
@@ -21,6 +21,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<EditPost />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route path='/profile/:id/*' element={<Profile />} />
+          <Route path='/update-profile/:id' element={<UpdateProfile />} />
         </Route>
       </Routes>
 
