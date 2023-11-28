5f1a48724c8917d3d66ca1c2789fdf88a8f4052f
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-28 03:35:57 +0100

    PostDetails

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -10,12 +10,12 @@ import { checkIsLiked } from '@/lib/utils'
 import Loader from './Loader'
 
 type PostStatsProps = {
-  post: Models.Document
+  post?: Models.Document
   userId: string
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-  const likesList = post.likes.map((user: Models.Document) => user.$id)
+  const likesList = post?.likes.map((user: Models.Document) => user.$id)
 
   const [likes, setLikes] = useState<string[]>(likesList)
   const [isSaved, setIsSaved] = useState(false)
@@ -41,11 +41,11 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     }
 
     setLikes(newLikes)
-    likePost({ postId: post.$id, likesArray: newLikes })
+    likePost({ postId: post?.$id || '', likesArray: newLikes })
   }
 
   const savedPostRecord = currentUser?.save.find(
-    (record: Models.Document) => record.post.$id === post.$id
+    (record: Models.Document) => record.post.$id === post?.$id
   )
 
   useEffect(() => {
@@ -60,7 +60,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
       return deleteSavePost(savedPostRecord.$id)
     }
 
-    savePost({ postId: post.$id, userId: userId })
+    savePost({ postId: post?.$id || '', userId: userId })
     setIsSaved(true)
   }
 
@@ -70,8 +70,8 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
         <img
           src={
             checkIsLiked(likes, userId)
-              ? './assets/icons/liked.svg'
-              : './assets/icons/like.svg'
+              ? '../assets/icons/liked.svg'
+              : '../assets/icons/like.svg'
           }
           alt='like'
           width={20}
@@ -87,7 +87,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
         ) : (
           <img
             src={
-              isSaved ? './assets/icons/saved.svg' : './assets/icons/save.svg'
+              isSaved ? '../assets/icons/saved.svg' : '../assets/icons/save.svg'
             }
             alt='save'
             width={20}
